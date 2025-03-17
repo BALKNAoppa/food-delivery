2c4a772e839f67efc0ef82f6df80c88d6b6a066c
BALKNAoppa <baterdene207@gmail.com>
2025-03-17 12:20:48 +0800

    someupdate

--- Backend/src/modules/foodCategoryModel.js
+++ Backend/src/modules/foodCategoryModel.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 const FoodCategorySchema = mongoose.Schema({
     categoryName: { type: String, required: true },
+    name: { type: String, required: true },
     createdAt: { type: Date, default: new Date() },
     updatedAt: { type: Date, default: Date.now },
-})
+});
 
-export const FoodCategoryModel = mongoose.models['foodCategory'] || mongoose.model('foodCategory', FoodCategorySchema)
+export const FoodCategoryModel = mongoose.models['foodCategory'] || mongoose.model('foodCategory', FoodCategorySchema);
