ccca9f22249544f16764ad8490b25e7564292250
BALKNAoppa <baterdene207@gmail.com>
2025-03-24 11:32:37 +0800

    someupdates

--- Backend/src/middleware/autho.js
+++ Backend/src/middleware/autho.js
@@ -1,16 +1,16 @@
 import jwt from "jsonwebtoken";
 
 export const authorizationMiddleware = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1]; // Extract token from header
+  const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ message: "Unauthorized: No token provided" });
   }
 
   try {
-    const decoded = jwt.verify(token, "uneheer nuuts"); // Verify token
-    req.user = decoded; // Store user info in request object
-    next(); // Move to the next middleware
+    const decoded = jwt.verify(token, "uneheer nuuts");
+    req.user = decoded;
+    next();
   } catch (err) {
     return res.status(403).json({ message: "Unauthorized: Invalid token" });
   }
