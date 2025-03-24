52a2396bbeda2f6e39feb351d3a5fab1e2e9a016
BALKNAoppa <baterdene207@gmail.com>
2025-03-24 10:07:20 +0800

    someupdate

--- Backend/src/routers/food-router.js
+++ Backend/src/routers/food-router.js
@@ -4,11 +4,11 @@ import { authorizationMiddleware } from "../middleware/autho.js";
 import { createFood } from "../controller/foods/create-food.js";
 import { getFoods } from "../controller/foods/get-foods.js";
 import { deleteFood } from "../controller/foods/delete-food.js";
-import { updateFood } from "../controller/foods/update-food.js";  // Import the new update function
+import { updateFood } from "../controller/foods/update-food.js";
 
 export const foodRouter = express.Router();
 
 foodRouter.get("/", getFoods);
 foodRouter.post("/createfood", authorizationMiddleware, createFood);
 foodRouter.post("/delete", authorizationMiddleware, deleteFood);
-foodRouter.put("/update/:id", authorizationMiddleware, updateFood);  // Use the correct update route with ID
+foodRouter.put("/update/:id", authorizationMiddleware, updateFood);
